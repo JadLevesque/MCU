b17d5bc75f41550020ce3fdf882d63b38a949e60
Etienne <etmic6@ulaval.ca>
2022-10-02 15:07:36 -0400

    Test multiple controller (NotWorking)
    Juste une mannette se connecte en meme temps mais au moins la manette fonction quand elle est connecter et c'est la premiere manette qui se connecte qui fonctionne. Les 2 mannettes fonctionnes quand elle sont connecté

--- robot-esp32/src/main.cpp
+++ robot-esp32/src/main.cpp
@@ -1,9 +1,253 @@
 #include <Arduino.h>
+#include <Ps3Controller.h>
 
-void setup() {
-  // put your setup code here, to run once:
+int player = 0;
+int battery = 0;
+Ps3Controller p1;
+Ps3Controller p2;
+
+void notify(Ps3Controller countroller)
+{
+    //--- Digital cross/square/triangle/circle button events ---
+    if( countroller.event.button_down.cross )
+        Serial.println("Started pressing the cross button");
+    if( countroller.event.button_up.cross )
+        Serial.println("Released the cross button");
+
+    if( countroller.event.button_down.square )
+        Serial.println("Started pressing the square button");
+    if( countroller.event.button_up.square )
+        Serial.println("Released the square button");
+
+    if( countroller.event.button_down.triangle )
+        Serial.println("Started pressing the triangle button");
+    if( countroller.event.button_up.triangle )
+        Serial.println("Released the triangle button");
+
+    if( countroller.event.button_down.circle )
+        Serial.println("Started pressing the circle button");
+    if( countroller.event.button_up.circle )
+        Serial.println("Released the circle button");
+
+    //--------------- Digital D-pad button events --------------
+    if( countroller.event.button_down.up )
+        Serial.println("Started pressing the up button");
+    if( countroller.event.button_up.up )
+        Serial.println("Released the up button");
+
+    if( countroller.event.button_down.right )
+        Serial.println("Started pressing the right button");
+    if( countroller.event.button_up.right )
+        Serial.println("Released the right button");
+
+    if( countroller.event.button_down.down )
+        Serial.println("Started pressing the down button");
+    if( countroller.event.button_up.down )
+        Serial.println("Released the down button");
+
+    if( countroller.event.button_down.left )
+        Serial.println("Started pressing the left button");
+    if( countroller.event.button_up.left )
+        Serial.println("Released the left button");
+
+    //------------- Digital shoulder button events -------------
+    if( countroller.event.button_down.l1 )
+        Serial.println("Started pressing the left shoulder button");
+    if( countroller.event.button_up.l1 )
+        Serial.println("Released the left shoulder button");
+
+    if( countroller.event.button_down.r1 )
+        Serial.println("Started pressing the right shoulder button");
+    if( countroller.event.button_up.r1 )
+        Serial.println("Released the right shoulder button");
+
+    //-------------- Digital trigger button events -------------
+    if( countroller.event.button_down.l2 )
+        Serial.println("Started pressing the left trigger button");
+    if( countroller.event.button_up.l2 )
+        Serial.println("Released the left trigger button");
+
+    if( countroller.event.button_down.r2 )
+        Serial.println("Started pressing the right trigger button");
+    if( countroller.event.button_up.r2 )
+        Serial.println("Released the right trigger button");
+
+    //--------------- Digital stick button events --------------
+    if( countroller.event.button_down.l3 )
+        Serial.println("Started pressing the left stick button");
+    if( countroller.event.button_up.l3 )
+        Serial.println("Released the left stick button");
+
+    if( countroller.event.button_down.r3 )
+        Serial.println("Started pressing the right stick button");
+    if( countroller.event.button_up.r3 )
+        Serial.println("Released the right stick button");
+
+    //---------- Digital select/start/ps button events ---------
+    if( countroller.event.button_down.select )
+        Serial.println("Started pressing the select button");
+    if( countroller.event.button_up.select )
+        Serial.println("Released the select button");
+
+    if( countroller.event.button_down.start )
+        Serial.println("Started pressing the start button");
+    if( countroller.event.button_up.start )
+        Serial.println("Released the start button");
+
+    if( countroller.event.button_down.ps )
+        Serial.println("Started pressing the Playstation button");
+    if( countroller.event.button_up.ps )
+        Serial.println("Released the Playstation button");
+
+
+    //---------------- Analog stick value events ---------------
+   if( abs(countroller.event.analog_changed.stick.lx) + abs(countroller.event.analog_changed.stick.ly) > 2 ){
+       Serial.print("Moved the left stick:");
+       Serial.print(" x="); Serial.print(countroller.data.analog.stick.lx, DEC);
+       Serial.print(" y="); Serial.print(countroller.data.analog.stick.ly, DEC);
+       Serial.println();
+    }
+
+   if( abs(countroller.event.analog_changed.stick.rx) + abs(countroller.event.analog_changed.stick.ry) > 2 ){
+       Serial.print("Moved the right stick:");
+       Serial.print(" x="); Serial.print(countroller.data.analog.stick.rx, DEC);
+       Serial.print(" y="); Serial.print(countroller.data.analog.stick.ry, DEC);
+       Serial.println();
+   }
+
+   //--------------- Analog D-pad button events ----------------
+   if( abs(countroller.event.analog_changed.button.up) ){
+       Serial.print("Pressing the up button: ");
+       Serial.println(countroller.data.analog.button.up, DEC);
+   }
+
+   if( abs(countroller.event.analog_changed.button.right) ){
+       Serial.print("Pressing the right button: ");
+       Serial.println(countroller.data.analog.button.right, DEC);
+   }
+
+   if( abs(countroller.event.analog_changed.button.down) ){
+       Serial.print("Pressing the down button: ");
+       Serial.println(countroller.data.analog.button.down, DEC);
+   }
+
+   if( abs(countroller.event.analog_changed.button.left) ){
+       Serial.print("Pressing the left button: ");
+       Serial.println(countroller.data.analog.button.left, DEC);
+   }
+
+   //---------- Analog shoulder/trigger button events ----------
+   if( abs(countroller.event.analog_changed.button.l1)){
+       Serial.print("Pressing the left shoulder button: ");
+       Serial.println(countroller.data.analog.button.l1, DEC);
+   }
+
+   if( abs(countroller.event.analog_changed.button.r1) ){
+       Serial.print("Pressing the right shoulder button: ");
+       Serial.println(countroller.data.analog.button.r1, DEC);
+   }
+
+   if( abs(countroller.event.analog_changed.button.l2) ){
+       Serial.print("Pressing the left trigger button: ");
+       Serial.println(countroller.data.analog.button.l2, DEC);
+   }
+
+   if( abs(countroller.event.analog_changed.button.r2) ){
+       Serial.print("Pressing the right trigger button: ");
+       Serial.println(countroller.data.analog.button.r2, DEC);
+   }
+
+   //---- Analog cross/square/triangle/circle button events ----
+   if( abs(countroller.event.analog_changed.button.triangle)){
+       Serial.print("Pressing the triangle button: ");
+       Serial.println(countroller.data.analog.button.triangle, DEC);
+   }
+
+   if( abs(countroller.event.analog_changed.button.circle) ){
+       Serial.print("Pressing the circle button: ");
+       Serial.println(countroller.data.analog.button.circle, DEC);
+   }
+
+   if( abs(countroller.event.analog_changed.button.cross) ){
+       Serial.print("Pressing the cross button: ");
+       Serial.println(countroller.data.analog.button.cross, DEC);
+   }
+
+   if( abs(countroller.event.analog_changed.button.square) ){
+       Serial.print("Pressing the square button: ");
+       Serial.println(countroller.data.analog.button.square, DEC);
+   }
+
+   //---------------------- Battery events ---------------------
+    if( battery != countroller.data.status.battery ){
+        battery = countroller.data.status.battery;
+        Serial.print("The controller battery is ");
+        if( battery == ps3_status_battery_charging )      Serial.println("charging");
+        else if( battery == ps3_status_battery_full )     Serial.println("FULL");
+        else if( battery == ps3_status_battery_high )     Serial.println("HIGH");
+        else if( battery == ps3_status_battery_low)       Serial.println("LOW");
+        else if( battery == ps3_status_battery_dying )    Serial.println("DYING");
+        else if( battery == ps3_status_battery_shutdown ) Serial.println("SHUTDOWN");
+        else Serial.println("UNDEFINED");
+    }
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void notify1(){
+    notify(p1);
+}
+
+void notify2(){
+    notify(p2);
+}
+
+void onConnect(){
+    Serial.println("Connected.");
+}
+
+void setup()
+{
+    Serial.begin(115200);
+
+    p1.attach(notify1);
+    p1.attachOnConnect(onConnect);
+    p1.begin("00:00:00:00:00:01");
+
+    p2.attach(notify2);
+    p2.attachOnConnect(onConnect);
+    p2.begin();
+
+    Serial.println("Ready.");
+}
+
+void loop()
+{
+    if(!p1.isConnected())
+        return;
+    p1.setPlayer(1);
+    if(!p2.isConnected())
+        return;
+    p2.setPlayer(2);
+
+
+    // //------ Digital cross/square/triangle/circle buttons ------
+    // if( p1.data.button.cross && p1.data.button.down )
+    //     Serial.println("Pressing both the down and cross buttons");
+    // if( p1.data.button.square && p1.data.button.left )
+    //     Serial.println("Pressing both the square and left buttons");
+    // if( p1.data.button.triangle && p1.data.button.up )
+    //     Serial.println("Pressing both the triangle and up buttons");
+    // if( p1.data.button.circle && p1.data.button.right )
+    //     Serial.println("Pressing both the circle and right buttons");
+
+    // if( p1.data.button.l1 && p1.data.button.r1 )
+    //     Serial.println("Pressing both the left and right bumper buttons");
+    // if( p1.data.button.l2 && p1.data.button.r2 )
+    //     Serial.println("Pressing both the left and right trigger buttons");
+    // if( p1.data.button.l3 && p1.data.button.r3 )
+    //     Serial.println("Pressing both the left and right stick buttons");
+    // if( p1.data.button.select && p1.data.button.start )
+    //     Serial.println("Pressing both the select and start buttons");
+
+    delay(2000);
 }
